e4e323f26f0fdc792b86b44164c071ebf2eff136
trashb <ashleybi2005@gmail.com>
2024-09-09 10:32:39 -0400

    Created Reduce.java

---
+++ src/Reduce.java
@@ -0,0 +1,13 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int count = 0;
+        while (i > 0){
+            if (i % 2 == 0) {
+                i = i / 2;
+            } else { i--; }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
