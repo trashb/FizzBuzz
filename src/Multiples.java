6903df9357e2de7ba5f26f34395ef9d7eb2eb191
trashb <ashleybi2005@gmail.com>
2024-09-09 10:22:59 -0400

    Created Multiples.java

---
+++ src/Multiples.java
@@ -0,0 +1,9 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i=3; i<1000; i++){
+            if (i % 3 == 0 || i % 5 == 0){ count += 1;}
+        }
+        System.out.println(count);
+    }
+}
